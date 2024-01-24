38a4448c4905ae831c7b258df204b211380d8231
spikerrbg <spikerrbg@gmail.com>
2024-01-24 11:28:24 +0200

    adding validation

--- src/components/OtpLogin.jsx
+++ src/components/OtpLogin.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react"
 
 
 export default function OtpLogin() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [phoneNumber, setPhoneNumber] = useState("")
+
+    const handlePhoneNumber = (e) => {
+        setPhoneNumber(e.target.value)
+    }
+    const handlePhoneSubmit = (e) => {
+        e.preventDefault()
+        //phone validation
+        const regex = /[^0-9]/g
+        if(phoneNumber.length<10 || regex.test(phoneNumber)){
+        alert("Invalid Phone Number ")
+    return
+}
+
+// connect to API
+// show OTP Fileds
+    }
+
+    return (
+        <div>
+            <form onSubmit={handlePhoneSubmit}>
+                <input type="text" value={phoneNumber} onChange={handlePhoneNumber}
+                    placeholder="Phone number" />
+                <button type="submit">Submit</button>
+
+            </form>
+        </div>
+    )
 }
